4b34ccacfb9a9d99910e3d14055074f8eb0d9a29
farisahcaca <farisah8525@gmail.com>
2025-04-27 12:33:38 +0700

    Menambahkan kegiatan baru

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,29 @@ const daftarKegiatan = ref([
   'Gym',
   'Traveling',
 ])
+// Data untuk input kegiatan baru
+const kegiatanBaru = ref('')
+
+// Function untuk tambah kegiatan
+function tambahKegiatan() {
+  if (kegiatanBaru.value.trim() !== '') {
+    daftarKegiatan.value.push(kegiatanBaru.value)
+    kegiatanBaru.value = '' // Clear input setelah tambah
+  }
+}
 </script>
 
 <template>
  <div class="app">
-    <h1>Daftar Kegiatan</h1>
+  <h1>📋MyActivity📌</h1>
+  <form @submit.prevent="tambahKegiatan">
+      <input
+        type="text"
+        v-model="kegiatanBaru"
+        placeholder="Tambah kegiatan..."
+      />
+      <button type="submit">Tambah</button>
+    </form>
     <ul>
       <li v-for="(kegiatan, index) in daftarKegiatan" :key="index">
         {{ kegiatan }}
